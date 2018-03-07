e3f41c6c0b8dffad084a8b50ca4fb2e1a152db38
Wenping Guo <ybyygu@gmail.com>
2018-03-07 10:34:44 +0800

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+// [[file:~/Workspace/Programming/rust-octree/rust-octree.note::7896c853-796e-4f97-b1d4-45546d4491db][7896c853-796e-4f97-b1d4-45546d4491db]]
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+// 7896c853-796e-4f97-b1d4-45546d4491db ends here
